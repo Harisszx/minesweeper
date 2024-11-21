867579e15a7cbcd69e1308f4d9610a9e5dc26c25
Haris Zafar <28100453@lums.edu.pk>
2024-11-21 18:08:38 +0500

    Code received by Muaz. The code seems fine. Just working on adding comments to the program.

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -71,9 +71,9 @@ public:
 class board
 {
 private:
-    static const int ROWS = 5; 
-    static const int COLUMNS = 5;
-    static const int TOTALMINES = 7;
+    static const int ROWS = 5; // Fixed number of ROWS - Change the rows, columns, and the mines in ratios to ensure the prpoer functionality of the game. 
+    static const int COLUMNS = 5; // Fixed number of the COLUMNS
+    static const int TOTALMINES = 7; // The total number of mines that will be placed on the board.
     tile grid[ROWS][COLUMNS];
     RenderWindow window; // SFML keyword to make a window on which we can display our graphics
     bool gameover;
@@ -82,12 +82,217 @@ public:
     board() : window(VideoMode(250,250), "Minesweeper") // Length, and Width of the window can be changed from here. Just increase it by the ratios as you increase the rows, and columns.
     {
         gameover = false; // Setting this condtion to false in the constructor to make sure that the program runs atleast once
+        initialize();
+        putadjacentmines();
     }
 
+    void reset() 
+    {
+        gameover = false;
+        initialize();
+        putadjacentmines();
+    }
+
+    void initialize() 
+    {
+        srand(time(0));
+        vector<int> mine_positions; // Dynamic array of integers that will store the positions of the mines on board
+        while (mine_positions.size() < TOTALMINES) // Making sure that everything is not a mine. :)
+        {
+            int pos = rand() % (ROWS * COLUMNS); // Creating a random number. The % determines that the number stays in the bounds
+            if (find(mine_positions.begin(), mine_positions.end(), pos) == mine_positions.end()) // find function looks for the pos in the mine_positions, and if not found returns mine_positions.end(). Then the condition will be true, it will randomly place a mine.
+            {
+                mine_positions.push_back(pos); // push_back adds the new position to the vector
+                int x = pos / COLUMNS; // calculates the row index
+                int y = pos % COLUMNS; // calculates the column index 
+                grid[x][y].place_mine(); // sets the mine to the (x,y) coordinate and calls the place_mine func that returns the ismine variable to true. Everytime it runs, it's going to mark change the status of one mine as true.
+            }
+        }
+    }
+
+    void putadjacentmines() 
+    {
+        for (int x = 0; x < ROWS; x++) 
+        {
+            for (int y = 0; y < COLUMNS; y++)
+            {
+                if (!grid[x][y].get_ismine()) 
+                {
+                    for (int dx = -1; dx <= 1; dx++) 
+                    {
+                        for (int dy = -1; dy <= 1; dy++) 
+                        {
+                            if (x + dx >= 0 && x + dx < ROWS && y + dy >= 0 && y + dy < COLUMNS && !(dx == 0 && dy == 0) && grid[x + dx][y + dy].get_ismine()) 
+                            {
+                                grid[x][y].set_adjacentmines(grid[x][y].get_adjacentmines() + 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void display_grid() 
+    {
+        Texture coveredTexture, flagTexture, bombTexture;
+        coveredTexture.loadFromFile("MINESWEEPER_X.png");
+        flagTexture.loadFromFile("MINESWEEPER_F.png");
+        bombTexture.loadFromFile("MINESWEEPER_M.png");
+
+        Texture uncoveredTexture[9];
+        for (int x = 0; x < 9; x++) 
+        {
+            uncoveredTexture[x].loadFromFile("MINESWEEPER_" + to_string(x) + ".png");
+        }
+
+        Font font;
+        if (!font.loadFromFile("Roboto-Black.ttf")) 
+        {
+            cout << "Download Roboto - Black font please." << endl;
+        }
+
+        while (window.isOpen()) 
+        {
+            Event this_event;
+            while (window.pollEvent(this_event)) 
+            {
+                if (this_event.type == Event::Closed) 
+                {
+                    window.close();
+                }
+
+                if (this_event.type == Event::MouseButtonPressed && !gameover) 
+                {
+                    int x_pos = this_event.mouseButton.x / 50;
+                    int y_pos = this_event.mouseButton.y / 50;
+
+                    if (x_pos >= 0 && x_pos < ROWS && y_pos >= 0 && y_pos < COLUMNS) 
+                    {
+                        if (this_event.mouseButton.button == Mouse::Left) 
+                        {
+                            if (!grid[x_pos][y_pos].get_isflagged()) 
+                            {
+                                uncover(x_pos, y_pos);
+                                if (grid[x_pos][y_pos].get_ismine()) 
+                                {
+                                    gameover = true;
+                                }
+                            }
+                        } 
+                        else if (this_event.mouseButton.button == Mouse::Right)
+                        {
+                            grid[x_pos][y_pos].toggle_flag();
+                        }
+                    }
+                }
+
+                if (this_event.type == Event::KeyPressed && gameover) {
+                    if (this_event.key.code == Keyboard::Y) {
+                        reset();
+                    } else if (this_event.key.code == Keyboard::N) {
+                        window.close();
+                    }
+                }
+            }
+
+            window.clear(Color::Black);
+
+            for (int x = 0; x < ROWS; x++) 
+            {
+                for (int y = 0; y < COLUMNS; y++) 
+                {
+                    int x_pos = x * 50;
+                    int y_pos = y * 50;
+                    Sprite tileSprite;
+
+                    if (grid[x][y].get_iscovered())
+                    {
+                        tileSprite.setTexture(coveredTexture);
+                        tileSprite.setScale(50.0f / tileSprite.getLocalBounds().width, 50.0f / tileSprite.getLocalBounds().height);
+                        tileSprite.setPosition(x_pos, y_pos);
+                        window.draw(tileSprite);
+
+                        if (grid[x][y].get_isflagged()) 
+                        {
+                            tileSprite.setTexture(flagTexture);
+                            tileSprite.setScale(50.0f / tileSprite.getLocalBounds().width, 50.0f / tileSprite.getLocalBounds().height);
+                            tileSprite.setPosition(x_pos, y_pos);
+                            window.draw(tileSprite);
+                        }
+                    } 
+                    else 
+                    {
+                        if (grid[x][y].get_ismine()) 
+                        {
+                            tileSprite.setTexture(bombTexture);
+                            tileSprite.setScale(50.0f / tileSprite.getLocalBounds().width, 50.0f / tileSprite.getLocalBounds().height);
+                            tileSprite.setPosition(x_pos, y_pos);
+                            window.draw(tileSprite);
+                        } 
+                        else 
+                        {
+                            tileSprite.setTexture(uncoveredTexture[grid[x][y].get_adjacentmines()]);
+                            tileSprite.setScale(50.0f / tileSprite.getLocalBounds().width, 50.0f / tileSprite.getLocalBounds().height);
+                            tileSprite.setPosition(x_pos, y_pos);
+                            window.draw(tileSprite);
+                        }
+                    }
+                }
+            }
+
+            if (gameover) 
+            {
+                Text gameOverText("Game Over!", font, 48);
+                gameOverText.setCharacterSize(36);
+                gameOverText.setFillColor(Color::Red);
+                gameOverText.setPosition((COLUMNS * 50) / 2 - 100, (ROWS * 50) / 2 - 50);
+                window.draw(gameOverText);
+
+                Text retryText("Do you want to retry? (y/n)", font, 24);
+                retryText.setPosition((COLUMNS * 50) / 2 - 100, (ROWS * 50) / 2 + 50);
+                window.draw(retryText);
+            }
+
+            window.display();
+        }
+    }
+
+    void uncover(int row, int column) 
+    {
+        if (row < 0 || row >= ROWS || column < 0 || column >= COLUMNS || !grid[row][column].get_iscovered()) 
+        {
+            return;
+        }
+
+        grid[row][column].uncover_tile();
+        
+        if (grid[row][column].get_ismine()) 
+        {
+            return;
+        }
+
+        if (grid[row][column].get_adjacentmines() > 0) 
+        {
+            return;
+        }
+
+        for (int dx = -1; dx <= 1; dx++) 
+        {
+            for (int dy = -1; dy <= 1; dy++) 
+            {
+                if (dx != 0 || dy != 0) 
+                {
+                    uncover(row + dx, column + dy);
+                }
+            }
+        }
+    }
 };
 
 int main()
 {
     board Board1; // Creates a object named Board1 with all the predefined properties in the board class;
-
+    Board1.display_grid();
+    return 0;
 }
